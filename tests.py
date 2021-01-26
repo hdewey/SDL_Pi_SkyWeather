071d41e153ebe7a5017ffb81701337fa21334bdf
hdewey <hdewey02@gmail.com>
2021-01-26 11:42:53 -0600

    added timelapse test

---
+++ tests.py
@@ -0,0 +1,4 @@
+import subprocess
+
+def createTimelapse():
+	subprocess.call('sudo node timelapse-test.js')
---
+++ timelapse-test.js
@@ -0,0 +1,66 @@
+// start at 5am...
+const fs = require('fs');
+const fsExtra = require('fs-extra')
+
+const videoshow = require('videoshow')
+const { Storage } = require('@google-cloud/storage');
+
+const photoPath = './static/timelapse/';
+const bucketName = 'sstx-timelapse';
+
+const projectId = 'skyweatherv3'
+const keyFilename = 'service-key.json'
+const storage = new Storage({projectId, keyFilename});
+
+const clean = () => {
+  const fs = require('fs').promises;
+  fs.rm('./static/timelapse.mp4');
+  
+  fsExtra.emptyDirSync(photoPath)
+}
+
+const run = (images) => {
+  var videoOptions = {
+    fps: 25,
+    loop: .1, // seconds
+    transition: false,
+    videoBitrate: 1024,
+    videoCodec: 'libx264',
+    size: '1920x1080',
+    format: 'mp4',
+    pixelFormat: 'yuv420p'
+  }
+
+  videoshow(images, videoOptions)
+    .save('static/timelapse.mp4')
+    .on('start', function (command) {
+      console.log('ffmpeg process started:', command)
+    })
+    .on('error', function (err, stdout, stderr) {
+      console.error('Error:', err)
+      console.error('ffmpeg stderr:', stderr)
+    })
+    .on('end', function (output) {
+      console.error('Video created in:', output)
+
+      clean()
+    })
+}
+
+const start = async () => {
+
+  fs.readdir(photoPath, (err, files) => {
+    let arr = [];
+
+    files.forEach(file => {
+      arr.push(photoPath + file)
+    })
+
+    const final = arr;
+
+    run(final);
+
+  })
+}
+
+start();
